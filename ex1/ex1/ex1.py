45c4babdba5641285832e6e503f59ac10089f27b
Kashmit <gurudubstar@gmail.com>
2017-12-04 19:33:19 +1300

    Study Drills ex

--- ex1/ex1/ex1.py
+++ ex1/ex1/ex1.py
@@ -1,7 +1,13 @@
-print ("Hello World!")
-print ("Hello Again")
-print ("I like to type this.")
-print ("This is fun.")
-print ("Yay! Printing")
-print ("I'd much rather you 'not'.")
-print ('I "said do not touch this.')
+
+if(True):
+    print ("Hello World!")
+else:
+    print ("Hello Again")
+    print ("I like to type this.")
+    print ("This is fun.")
+    print ("Yay! Printing")
+    print ("I'd much rather you 'not'.")
+    print ('I "said" do not touch this.')
+
+
+#print ("test") '#' is used to comment
