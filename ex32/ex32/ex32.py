55bcf16f25b7db89f0929b95b3d1880ff9d7874c
Kashmit <gurudubstar@gmail.com>
2018-02-11 12:41:54 +1200

    ex32 loops

---
+++ ex32/ex32/ex32.py
@@ -0,0 +1,11 @@
+the_count = [1,2,3,4,5]
+fruits = ['apples', 'orange']
+change = [1, 'pennies']
+
+for number in the_count:
+	print(number)
+
+for fruit in fruits:
+	print(f"A fruit of type: {fruit}")
+
+
