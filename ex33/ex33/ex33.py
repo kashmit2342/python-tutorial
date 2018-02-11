2eff708be354df0ae3fa392fc961c08ebd0ee43c
Kashmit <gurudubstar@gmail.com>
2018-02-11 12:49:50 +1200

    ex33 while loop

---
+++ ex33/ex33/ex33.py
@@ -0,0 +1,16 @@
+i = 0
+numbers = []
+
+while i < 6:
+	print(f"number appended is:{i}")
+	numbers.append(i)
+	i+=1
+	print(f"i now is:{i}")
+
+for number in numbers:
+
+	print(number)
+
+print(f"all numbers {numbers}")
+
+
